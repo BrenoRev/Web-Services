53568a7f2cf073757673578a354ebe6c82a35391
Breno Rev <silvabreno462@gmail.com>
2021-06-11 20:31:42 -0300

    H2 Database / JPA / Test Profile

---
+++ src/main/java/br/com/educandoweb/course/CourseDevApplication.java
@@ -0,0 +1,13 @@
+package br.com.educandoweb.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseDevApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,23 @@
+package br.com.educandoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.educandoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	// Retorna todos os usuarios para a pagina
+	// localhost:8080/users
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L , "Maria" , "maria@gmail.com" , "99999-9999" ,"12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
